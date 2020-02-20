a5dd5ebe64541579f7b4f281d2b3a6905f3db678
v.blokhin <v.blokhin>
2020-02-20 21:56:26 +0200

    another commit

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 import {PickerViewProps} from './src/components/nativePicker/NativePicker.interface';
+import React from 'react';
+
 declare module 'trstlr-components';
 
-export var HorizontalPicker: PickerViewProps;
+export var HorizontalPicker: React.FC<PickerViewProps>;
